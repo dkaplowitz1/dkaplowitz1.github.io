d4997d8692af8e4d95ea05c3e3fd9fa6ff3c6c7f
dkaplowitz1 <dylan_kaplowitz@yahoo.com>
2021-05-06 13:35:52 -0400

    initial push

---
+++ script.js
@@ -0,0 +1,15 @@
+(function(){
+    var words = [
+        ' Digital Illustrator',
+        ' Woodworker',
+        ' Musician'
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+      
+})();
+
